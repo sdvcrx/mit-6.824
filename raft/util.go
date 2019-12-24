7fd2902bc5d1fad378a746810ea45d70424504b8
sdvcrx <memory.silentvoyage@gmail.com>
2019-12-24 21:05:36 +0800

    feat(raft): add random util

--- raft/util.go
+++ raft/util.go
@@ -1,13 +1,21 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"math/rand"
+)
 
 // Debugging
-const Debug = 0
+const Debug = 1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
 		log.Printf(format, a...)
 	}
 	return
+}
+
+// Generate int with range in [from, to)
+func RandomRange(from, to int) int {
+	return rand.Intn(to-from) + from
 }
