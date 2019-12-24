f12943c612626030f171458e96f741ca82e872ae
sdvcrx <memory.silentvoyage@gmail.com>
2019-12-24 21:07:37 +0800

    feat(raft): add raft rpc

--- raft/raft_rpc.go
+++ raft/raft_rpc.go
@@ -1,5 +1,9 @@
 package raft
 
+import (
+	"time"
+)
+
 //
 // example RequestVote RPC arguments structure.
 // field names must start with capital letters!
@@ -27,6 +31,26 @@ type RequestVoteReply struct {
 //
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (2A, 2B).
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	currentTerm := rf.currentTerm
+
+	reply.Term = currentTerm
+
+	if currentTerm > args.Term {
+		reply.VoteGranted = false
+		return
+	}
+
+	if rf.VoteFor != VoteForNull {
+		reply.VoteGranted = false
+		return
+	}
+
+	rf.VoteFor = args.Term
+	reply.VoteGranted = true
+	return
 }
 
 //
@@ -61,4 +85,51 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
 	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
 	return ok
+}
+
+type AppendEntriesArgs struct {
+	Term      int
+	NextIndex int
+	Entries   []LogEntry
+}
+
+type AppendEntriesReply struct {
+	AppendSuccess bool // append successed or rejected
+}
+
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	DPrintf("Raft(%d) received", rf.me)
+	if len(args.Entries) == 0 {
+		// handle heartbeat
+		rf.lastHeartbeat = time.Now()
+		return
+	}
+}
+
+func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
+	ok := rf.peers[server].Call("Raft.AppendEntries", args, reply)
+	return ok
+}
+
+func (rf *Raft) sendHeartbeat() {
+	if rf.state != StateLeader {
+		return
+	}
+
+	rf.mu.Lock()
+	term := rf.currentTerm
+	peersLength := len(rf.peers)
+	rf.mu.Unlock()
+
+	args := &AppendEntriesArgs{
+		Term:    term,
+		Entries: []LogEntry{},
+	}
+
+	for i := 0; i < peersLength; i++ {
+		if i == rf.me {
+			continue
+		}
+		go rf.sendAppendEntries(i, args, &AppendEntriesReply{})
+	}
 }
