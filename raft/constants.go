b128d7fd315229fc2caad2ea6a83409ec2a77387
sdvcrx <memory.silentvoyage@gmail.com>
2020-02-10 23:37:44 +0800

    feat(raft): refine RaftState

--- raft/constants.go
+++ raft/constants.go
@@ -1,5 +1,9 @@
 package raft
 
+import (
+	"strings"
+)
+
 type RaftState int
 
 const (
@@ -7,5 +11,31 @@ const (
 	StateLeader
 	StateCandidate
 )
+
+func (rs RaftState) String() string {
+	switch rs {
+	case StateLeader:
+		return "Leader"
+	case StateCandidate:
+		return "Candidate"
+	case StateFollower:
+		return "Follower"
+	}
+	return "Follower"
+}
+
+func (rs RaftState) Is(role string) bool {
+	rl := strings.ToLower(role)
+
+	switch rl {
+	case "leader":
+		return rs == StateLeader
+	case "candidate":
+		return rs == StateCandidate
+	case "follower":
+		return rs == StateFollower
+	}
+	return rs == StateFollower
+}
 
 const VoteForNull = -1
